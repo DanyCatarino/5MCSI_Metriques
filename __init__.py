10956964961d9b277be1cf69243735ce0ece6692
Dany <55543221+DanyCatarino@users.noreply.github.com>
2024-02-12 17:23:36 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -15,18 +15,6 @@ def hello_world():
 def MaPremiereAPI():
     return render_template("contact.html")
 
-@app.route('/paris/')
-def meteo():
-    response = urlopen('https://api.openweathermap.org/data/2.5/forecast/daily?q=Paris,fr&cnt=16&appid=bd5e378503939ddaee76f12ad7a97608')
-    raw_content = response.read()
-    json_content = json.loads(raw_content.decode('utf-8'))
-    results = []
-    for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')
-        temp_day_value = list_element.get('temp', {}).get('day') - 273.15 # Conversion de Kelvin en °c 
-        results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)
-
 @app.route("/rapport/")
 def mongraphique():
     return render_template("graphique.html")
@@ -35,8 +23,8 @@ def mongraphique():
 def histogramme():
     return render_template("histogramme.html")
 
-@app.route('/previsions/')
-def previsions():
+@app.route('/paris/')
+def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
